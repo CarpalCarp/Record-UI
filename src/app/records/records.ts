a012d3668f53e753cbe56b1948451ab4bc9f2ad0
Matias Galante <CarpalCarp@users.noreply.github.com>
2025-08-11 15:16:00 -0600

    Updated the angular dependencies to add the angular animations module. The expanded details row is now able to expand with an animation

--- src/app/records/records.ts
+++ src/app/records/records.ts
@@ -23,7 +23,9 @@ interface TableRecord extends RawRecord {
   styleUrl: './records.scss',
   animations: [
     trigger('detailExpand', [
-
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ])
   ]
 })
