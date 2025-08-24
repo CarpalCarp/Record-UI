21d8353d9b33d22d50b2dd6885273ccf52a05bea
Matias Galante <CarpalCarp@users.noreply.github.com>
2025-08-24 10:43:15 -0600

    The records table now spans the full height of the page

--- src/app/records/records.component.ts
+++ src/app/records/records.component.ts
@@ -1,28 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RawRecord } from '../services/record.service';
 import { CommonModule } from '@angular/common';
 import { AgGridAngular } from 'ag-grid-angular';
-import { ModuleRegistry, ColDef, GridApi } from 'ag-grid-community';
+import { ModuleRegistry, ColDef } from 'ag-grid-community';
 import { AllEnterpriseModule } from 'ag-grid-enterprise';
 import { AgChartsEnterpriseModule } from 'ag-charts-enterprise';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 ModuleRegistry.registerModules([AllEnterpriseModule.with(AgChartsEnterpriseModule)]);
-
-type GroupingOptions = 'firstName' | 'lastName' | 'age' | 'noGrouping';
-
-interface GroupingSelection {
-  value: GroupingOptions
-  viewValue: string
-}
 
 @Component({
   selector: 'app-records',
   imports: [
-    MatFormFieldModule,
-    MatSelectModule,
     CommonModule,
     AgGridAngular
   ],
@@ -33,33 +22,13 @@ export class Records {
   dataSource: RawRecord[] = [];
   displayedColumns: ColDef[] = [
     { field: 'id', flex: 1, hide: true },
-    { field: 'firstName', flex: 1 },
-    { field: 'lastName', flex: 1 },
-    { field: 'age', flex: 1 }
+    { field: 'firstName', enableRowGroup: true, flex: 1 },
+    { field: 'lastName', enableRowGroup: true, flex: 1 },
+    { field: 'age', enableRowGroup: true, flex: 1 }
   ];
-  groups: GroupingSelection[] = [
-    { value: 'firstName', viewValue: 'First Name' },
-    { value: 'lastName', viewValue: 'Last Name' },
-    { value: 'age', viewValue: 'Age' }
-  ];
-  groupSelection = signal('noGrouping' as GroupingOptions);
-
-  private gridApi!: GridApi<RawRecord>;
+  rowGroupPanelShow: 'always' | 'onlyWhenGrouping' | 'never' = 'always';
 
   constructor(private activatedRoute: ActivatedRoute) {
     this.dataSource = this.activatedRoute.snapshot.data['records'];
-  }
-
-  changeTable(group: GroupingOptions) {
-    const updatedColData = this.displayedColumns.map(col => {
-      if (col.field === group) {
-        return { ...col, rowGroup: true };
-      } else {
-        return { ...col, rowGroup: false };
-      }
-    });
-    this.groupSelection.set(group);
-    this.displayedColumns = updatedColData;
-    this.gridApi.setGridOption("rowData", this.dataSource);
   }
 }
