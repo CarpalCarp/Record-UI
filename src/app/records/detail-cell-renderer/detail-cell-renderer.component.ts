119e763d830aaf6f278f9857cf805b7f93a81f93
Matias Galante <CarpalCarp@users.noreply.github.com>
2025-08-26 16:56:17 -0600

    Updated detail-cell-renderer to receive the data to display in the html

--- src/app/records/detail-cell-renderer/detail-cell-renderer.component.ts
+++ src/app/records/detail-cell-renderer/detail-cell-renderer.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
+import { GridApi, ICellRendererParams } from 'ag-grid-enterprise';
 
 @Component({
   selector: 'app-detail-cell-renderer',
@@ -7,8 +9,17 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './detail-cell-renderer.component.html',
   styleUrl: './detail-cell-renderer.component.scss'
 })
-export class DetailCellRenderer {
-  agInit(params: any): void { }
+export class DetailCellRenderer implements ICellRendererAngularComp {
+  params!: ICellRendererParams;
+  masterGridApi!: GridApi;
+  rowData!: any[];
+
+  agInit(params: any): void {
+    this.params = params;
+    this.masterGridApi = params.api;
+
+    this.rowData = params.data.callRecords;
+  }
 
   refresh(params: any): boolean {
     return false;
